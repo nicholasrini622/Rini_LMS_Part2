f83283db761dd2eeaf4580ecc9f813a9b9f25f08
Nicholas Rini <nicholasrini622@gmail.com>
2026-06-19 15:43:26 -0400

    Added comments for helper and Patron class.

--- src/LibraryManagementHelper.java
+++ src/LibraryManagementHelper.java
@@ -1,3 +1,9 @@
+//Nicholas Rini
+//CEN3024 - Software Development 1
+//06/19/2026
+//LibraryManagementHelper.java
+//This class will store and manage patron records.  Helper can add, remove by ID, and display/validate patron information
+
 import java.util.ArrayList;
 
 public class LibraryManagementHelper {
@@ -6,6 +12,13 @@ public class LibraryManagementHelper {
     public LibraryManagementHelper(){
         patrons = new ArrayList<>();
     }
+    /*
+    * Method: addPatron
+    * Parameters: Patron patron
+    * return: boolean
+    * purpose: Patron validation, check duplicate ID, add patron if valid.
+    * return true or false if patron was/wasn't added.
+     */
     public boolean addPatron(Patron patron){
         if(patron == null){
             return false;
@@ -23,6 +36,13 @@ public class LibraryManagementHelper {
         return true;
 
     }
+    /*
+    * Method: removePatron
+    * Parameters: String patronId
+    * Return: boolean
+    * Purpose: Find a patron based on unique ID and remove if found.
+    * Return true or false if patron was/wasn't removed.
+     */
     public boolean removePatron(String patronId){
         Patron patron = findPatron(patronId);
         if(patron != null){
@@ -31,6 +51,13 @@ public class LibraryManagementHelper {
         }
         return false;
     }
+    /*
+    * Method: findPatron
+    * Parameters: String patronId
+    * Return: Patron
+    * Purpose: Search list of patrons for matching ID
+    * Return Patron object if found, or null.
+     */
     public Patron findPatron(String patronId){
         for(Patron patron : patrons){
             if(patron.getPatronId().equals(patronId)){
@@ -39,9 +66,12 @@ public class LibraryManagementHelper {
         }
         return null;
     }
-    public ArrayList<Patron> getAllPatrons(){
-        return patrons;
-    }
+    /*
+    * Method: displayPatrons
+    * Parameters: none
+    * Return: void
+    * Purpose: Display all patrons, if patron list is empty and no patrons exist notify librarian.
+     */
     public void displayPatrons(){
         if(patrons.isEmpty()){
             System.out.println("Patrons empty, please add a patron.");
@@ -51,11 +81,21 @@ public class LibraryManagementHelper {
             System.out.println(patron);
         }
     }
-
+    /*
+    * Method: isValidFine
+    * Parameters: double overdueFine
+    * Return: boolean
+    * Purpose: Validation for fine amount, range must be between 0-250
+     */
     private boolean isValidFine(double overdueFine) {
         return overdueFine >= 0 && overdueFine <= 250;
     }
-
+    /*
+    * Method: isValidId
+    * Parameters: String patronId
+    * Return: boolean
+    * Purpose: Validation for patron ID, patrons must have a 7 digit unique ID.
+     */
     private boolean isValidId(String patronId) {
         if(patronId == null){
             return false;
--- src/Patron.java
+++ src/Patron.java
@@ -1,3 +1,9 @@
+//Nicholas Rini
+//CEN3024 - Software Development 1
+//06/19/2026
+//Patron.java
+//This class represents a patron of the library.  Each Patron must have a uniqure ID, name, address, and overdue fine
+
 public class Patron {
     private String patronId;
     private String name;
@@ -23,6 +29,7 @@ public class Patron {
     public double getOverdueFine(){
         return overdueFine;
     }
+    //Set Methods
     public void setName(String name){
         this.name = name;
     }
@@ -32,7 +39,7 @@ public class Patron {
     public void setOverdueFine(double overdueFine){
         this.overdueFine = overdueFine;
     }
-
+    //Formatting for patron display
     @Override
     public String toString() {
         return "Patron{" +
